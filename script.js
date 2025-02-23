3f086ba9cf309963aab1845f41e479b303bb4f5e
Srinath Vatchavari Venkateshan <166697624+vvsrinath@users.noreply.github.com>
2025-02-23 20:19:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(event) {
+        event.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        document.getElementById(targetId).scrollIntoView({ behavior: 'smooth' });
+    });
+});
